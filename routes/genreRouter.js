f1218a2122d5be84ef483772458f21478d41476a
Samith-hegde <hegdesamith02@gmail.com>
2024-11-07 22:54:56 +0530

    Add gamesForm and developers views

--- routes/genreRouter.js
+++ routes/genreRouter.js
@@ -4,8 +4,5 @@ const genreController = require('../controllers/genreController');
 const genreRouter = Router();
 
 genreRouter.get('/', genreController.getAllGenres);
-genreRouter.get('/add', genreController.getAddGenre);
-genreRouter.post('/add', genreController.postAddGenre);
-genreRouter.post('/delete/:id', genreController.postDeleteGenre);
 
 module.exports = genreRouter;
