882ebc53aac7a8457e3639c47ac02dabbb4c54f3
Samith-hegde <hegdesamith02@gmail.com>
2024-11-09 13:24:14 +0530

    Fix inconsistency in file names and imports

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const indexRouter = require('./routes/indexRouter');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+
 app.use('/', indexRouter);
 
 const PORT = process.env.PORT || 3000;
