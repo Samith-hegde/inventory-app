adeeefbeafc2ce7a2d5d3a28df7a1b7947beb9af
Samith-hegde <hegdesamith02@gmail.com>
2024-11-10 16:20:34 +0530

    Fix errors in queries

--- models/queries.js
+++ models/queries.js
@@ -1,13 +1,32 @@
 const pool = require('./pool');
 
 async function getAllGames() {
-    const query = 'SELECT * FROM games';
+    const query = `
+    SELECT g.*, 
+           array_agg(DISTINCT gn.genre) AS genres,
+           d.name AS developer_name
+    FROM game g
+    JOIN genre gn ON g.game_id = gn.game_id
+    JOIN developer d ON g.developer_id = d.developer_id
+    GROUP BY g.game_id, d.name`;
+  
+  try {
     const { rows } = await pool.query(query);
-    return rows;
+  
+    const allGames = rows.map(game => ({
+      ...game,
+      developer: game.developer_name,
+      genres: game.genres || []
+    }));
+  
+    return allGames;
+  } catch (error) {
+    console.error('Error fetching games:', error);
+  }
 }
 
 async function getGame(id) {
-    const query = 'SELECT * FROM games WHERE id = $1';
+    const query = 'SELECT * FROM game WHERE id = $1';
     const { rows } = await pool.query(query, [id]);
     return rows[0];
 }
@@ -15,58 +34,68 @@ async function getGame(id) {
 async function addGame(gameDetails) {
     const selectedDeveloper = gameDetails.developer;
     const developerID = await getDeveloperID(selectedDeveloper);
-    const query = 'INSERT INTO games (name, developer_id, price, description) VALUES ($1, $2)';
-    await pool.query(query, [gameDetails.name, developerID, gameDetails.price, gameDetails.description]);
+
+    const query_game = 'INSERT INTO game (name, developer_id, price, description) VALUES ($1, $2, $3, $4)';
+    await pool.query(query_game, [gameDetails.name, developerID, gameDetails.price, gameDetails.description]);
+
+    const query_game_id = 'SELECT game_id FROM game WHERE name = $1';
+    const { rows } = await pool.query(query_game_id, [gameDetails.name]);
+    const game_id = rows[0].game_id;
+
+    const query_genre = 'INSERT INTO genre (game_id, genre) VALUES ($1, $2)';
+    gameDetails.genres.forEach(async genre => {
+        await pool.query(query_genre, [game_id, genre]);
+    });
 }
 
 async function updateGame(id, gameDetails) {
     const selectedDeveloper = gameDetails.developer;
     const developerID = await getDeveloperID(selectedDeveloper);
-    const query = 'UPDATE games SET name = $2, developer_id = $3, price = $4, description = $5 WHERE id = $1';
+    const query = 'UPDATE game SET name = $2, developer_id = $3, price = $4, description = $5 WHERE id = $1';
     await pool.query(query, [id, gameDetails.name, developerID, gameDetails.price, gameDetails.description]);
 }
 
 async function deleteGame(id) {
-    const query = 'DELETE FROM games WHERE id = $1';
+    const query = 'DELETE FROM game WHERE id = $1';
     await pool.query(query, [id]);
 }
 
 async function getAllGenres() {
-    const query = 'SELECT DISTINCT genre FROM genres';
+    const query = 'SELECT DISTINCT genre FROM genre';
     const { rows } = await pool.query(query);
     return rows;
 }
 
 async function getAllDevelopers() {
-    const query = 'SELECT * FROM developers';
+    const query = 'SELECT * FROM developer';
     const { rows } = await pool.query(query);
     return rows;
 }
 
 async function getDeveloper(id) {
-    const query = 'SELECT * FROM developers WHERE id = $1';
+    const query = 'SELECT * FROM developer WHERE id = $1';
     const { rows } = await pool.query(query, [id]);
     return rows[0];
 }
 
 async function getDeveloperID(name) {
-    const query = 'SELECT id FROM developers WHERE name = $1';
+    const query = 'SELECT developer_id FROM developer WHERE name = $1';
     const { rows } = await pool.query(query, [name]);
-    return rows[0].id;
+    return rows[0].developer_id;
 }
 
 async function addDeveloper(developerDetails) {
-    const query = 'INSERT INTO developers (name, founded, location) VALUES ($1, $2)';
+    const query = 'INSERT INTO developer (name, founded, location) VALUES ($1, $2, $3)';
     await pool.query(query, [developerDetails.name, developerDetails.founded, developerDetails.location]);
 }
 
 async function updateDeveloper(id, developerDetails) {
-    const query = 'UPDATE developers SET name = $2, founded = $3, location = $4 WHERE id = $1';
+    const query = 'UPDATE developer SET name = $2, founded = $3, location = $4 WHERE id = $1';
     await pool.query(query, [id, developerDetails.name, developerDetails.founded, developerDetails.location]);
 }
 
 async function deleteDeveloper(id) {
-    const query = 'DELETE FROM developers WHERE id = $1';
+    const query = 'DELETE FROM developer WHERE id = $1';
     await pool.query(query, [id]);
 }
 
